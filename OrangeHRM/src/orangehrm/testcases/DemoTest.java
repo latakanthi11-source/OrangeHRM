a3850e829409b7c1a616a0a13d52ca2086129aea
pradh <pradh@LAPTOP-HT7BFMRF>
2026-02-18 21:19:07 +0530

    testcase added

---
+++ OrangeHRM/src/orangehrm/testcases/DemoTest.java
@@ -0,0 +1,21 @@
+package orangehrm.testcases;
+
+public class DemoTest 
+{
+
+	String url; // Global Variables
+	
+	void f1()
+	{
+		url = "http://google.com";
+		System.out.println(url);
+		
+		int x;	// Local Variables	
+	}
+	
+	
+	void f2()
+	{
+		System.out.println(url);			
+	}		
+}
